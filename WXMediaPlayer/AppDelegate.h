7854a199ee12f79e9015244b4f44d221fcb00840
nali <will.xu@ximalaya.com>
2018-03-21 15:04:08 +0800

    Initial Commit

---
+++ WXMediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXMediaPlayer
+//
+//  Created by will.xu on 2018/3/21.
+//  Copyright © 2018年 will.xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
